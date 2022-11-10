5d633dd1087077dd638d0859e34ba3286d0972d8
J. Campbell <75322448+j-campbell8778@users.noreply.github.com>
2022-11-10 18:32:23 +0900

    Add files via upload

--- src/english_please.py
+++ src/english_please.py
@@ -1,37 +1,41 @@
 """
 __filename__ = "english_please.py"
 ___author__ = "Jessie Campbell"
-__copyright__ = "None"
+__copyright__ = "Jessie Campbell"
 __credits__ = ["Jessie Campbell"]
 __license__ = "MIT"
 __version__ = "0.0.1"
 __maintainer__ = "Jessie Campbell"
 __email__ = "jessie.t.campbell@gmail.com"
-__status__ = "Test"
+__status__ = "Alpha"
 
 Module to move the mouse to a specified location series, and click the left mouse button,
 to interact with a known context menu.
 
-
 Uses the following functions:
 *main - Main loop of the module
-*move_to_1 - Moves mouse to location 1.
-*move_to_2 - Moves mouse to location 1.
-*move_to_3 - Moves mouse to location 1.
-*move_to_4 - Moves mouse to location 1.
-*click - Clicks the mouse on call.
+*move_to_1 - Moves mouse to location 1, with random delay.
+*move_to_2 - Moves mouse to location 1, with random delay.
+*move_to_3 - Moves mouse to location 1, with random delay.
+*move_to_4 - Moves mouse to location 1, with random delay.
+*click - Clicks the mouse on call, with random delay.
+*action - Runs the sequence of events.
+*test_interface - Testing interface for manual control via CLI.
+*find_path_2 - Finds the end point to aim path 2 movement at.
 
 Requires the following imports:
 -pyautogui
 -random
+-time
+-cv2 (opencv)
 
 Requires the following files to be present in the same directory as the module.
 -
 """
-import pyautogui
 import random
-import time
 from random import randrange
+import time
+import pyautogui
 
 
 def move_to_1():
@@ -44,6 +48,9 @@ def move_to_1():
     """
     x_cord = 212
     y_cord = 829
+    # obj = "./graphics/p1_target_obj.png"
+    # x_cord = find_path(obj)[0]
+    # y_cord = find_path(obj)[1]
     # movement time in seconds
     delay_base = 0.25
     # degrade of the base delay, a random floating point number
@@ -64,8 +71,9 @@ def move_to_2():
     1 crossing stop sign like icon.
     :return: None
     """
-    x_cord = 641
-    y_cord = 479
+    obj = "./graphics/p2_target_obj_with_lines.png"
+    x_cord = find_path(obj)[0]
+    y_cord = find_path(obj)[1]
     # movement time in seconds
     delay_base = 0.25
     # degrade of the base delay, a random floating point number
@@ -73,8 +81,8 @@ def move_to_2():
     delay = delay_base - delay_degrade
     # degrade of the base position, a random int
     pos_degrade = randrange(0, 3)
-    x_cord_f = x_cord - pos_degrade
-    y_cord_f = y_cord - pos_degrade
+    x_cord_f = x_cord + 10 - pos_degrade
+    y_cord_f = y_cord + 15 - pos_degrade
     pyautogui.moveTo(x_cord_f, y_cord_f, delay)
 
 
@@ -125,45 +133,51 @@ def move_to_4():
 def click_local():
     """
     Method to click the mouse when at the required location.
-    :return:
+    :return: None
     """
     # base movement time in seconds, subject to the delay degrade below
     delay_base = 0.05
     # degrade of the base delay, a random floating point number
-    delay_degrade = random.uniform(0.001, 0.004)
+    delay_degrade = random.uniform(0.0001, 0.0004)
     delay = delay_base - delay_degrade
-
+    time.sleep(delay)
     pyautogui.click()
 
 
 def action():
+    """
+    Method to run the sequence.
+    :return: None
+    """
+    pyautogui.PAUSE = 0.0025
     move_to_1()
-    time.sleep(0.075)
+    time.sleep(0.025)
     click_local()
-    time.sleep(0.075)
+    time.sleep(.55)
     move_to_2()
-    time.sleep(0.075)
+    time.sleep(0.025)
     click_local()
-    time.sleep(0.075)
+    time.sleep(0.025)
     move_to_3()
-    time.sleep(0.075)
+    time.sleep(0.025)
     click_local()
-    time.sleep(0.075)
+    time.sleep(0.025)
     move_to_4()
-    time.sleep(0.075)
+    time.sleep(0.025)
     click_local()
-    time.sleep(0.075)
-
-
-def main():
-    action()
-    #test_interface()
+    time.sleep(0.025)
 
 
 def test_interface():
+    """
+    Testing interface for CLI control.
+
+    This method opens a CLI to request user input to either start the sequence by calling
+    the action(), or exits the program.
+    :return: None
+    """
     print("Lets go")
     running = True
-    var = "0"
     while running is True:
         var = input("\nHit 1 to start, 9 to exit.\n")
         if var == "1":
@@ -173,6 +187,25 @@ def test_interface():
         else:
             time.sleep(1)
             print("Waiting")
+
+
+def find_path(obj):
+    """
+    This function will locate the coordinates for path 2.
+
+    This function calls the locate function from pyautogui, and returns the location of the
+    target x and y coordinates.
+    :return: cord (tuple)
+    """
+    cord = pyautogui.locateOnScreen(obj, confidence=0.8)
+    return cord
+
+
+def main():
+    """Main Method of the program."""
+    action()
+    # test_interface()
+    # pyautogui.moveTo(find_path_2())
 
 
 if __name__ == "__main__":
